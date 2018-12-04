b6c8671c4a6538b4076f6a343909eee84ddb3a68
geroza <rozagerardo@gmail.com>
2018-12-04 11:53:14 -0200

    Assembling the flux only on FooService class for readability purposes. To be analyzed

--- spring-5-reactive/src/main/java/com/baeldung/debugging/consumer/service/FooQuantityHelper.java
+++ spring-5-reactive/src/main/java/com/baeldung/debugging/consumer/service/FooQuantityHelper.java
@@ -4,28 +4,22 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import com.baeldung.debugging.consumer.model.Foo;
 
-import reactor.core.publisher.Flux;
-
 public class FooQuantityHelper {
 
-    public static Flux<Foo> processFooReducingQuantity(Flux<Foo> flux) {
-        flux = flux.map(foo -> {
-            Integer result;
-            Integer random = ThreadLocalRandom.current()
-                .nextInt(0, 90);
-            result = (random == 0) ? result = 0 : foo.getQuantity() + 2;
-            foo.setQuantity(result);
-            return foo;
-        });
-        return divideFooQuantity(flux);
+    public static Foo processFooReducingQuantity(Foo foo) {
+
+        Integer result;
+        Integer random = ThreadLocalRandom.current()
+            .nextInt(0, 90);
+        result = (random == 0) ? result = 0 : foo.getQuantity() + 2;
+        foo.setQuantity(result);
+        return divideFooQuantity(foo);
     }
 
-    public static Flux<Foo> divideFooQuantity(Flux<Foo> flux) {
-        return flux.map(foo -> {
-            Integer result = Math.round(5 / foo.getQuantity());
-            foo.setQuantity(result);
-            return foo;
-        });
+    public static Foo divideFooQuantity(Foo foo) {
+        Integer result = Math.round(5 / foo.getQuantity());
+        foo.setQuantity(result);
+        return foo;
     }
 
 }
